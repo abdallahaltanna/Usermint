cd11f1c51c470989012e00128596d02e80fc774d
Abdallah Altanna <a.altanna@vardot.com>
2025-05-20 23:24:10 +0300

    chore: install react-query, axios, react-toastify, and MUI dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { ToastContainer } from 'react-toastify'
+
 import './App.css'
 
 function App() {
-  return <h1>Hello world</h1>
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <ToastContainer />
+    </div>
+  )
 }
 
 export default App
